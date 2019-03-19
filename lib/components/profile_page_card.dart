1fb7234fb10de10e56f60f963bbe7de798d99635
Your Namesonam <you@example.comsonam.mandyweb@gmail.com>
2019-03-19 14:39:45 +0530

    19/3/2019 changes to ui of edit page

--- lib/components/profile_page_card.dart
+++ lib/components/profile_page_card.dart
@@ -6,10 +6,9 @@ class ProfilePageCard extends StatefulWidget {
 }
 
 class ProfilePageCardState extends State<ProfilePageCard> {
-
   String _emailid = 'chaudharysonam17@gmail.com';
   String _name = 'Sonam Chaudhary';
-  int _age = 24;
+  String _age = '24';
   String _qualification10 = 'punjab university';
   String _qualification12 = 'Non-medical';
   String _qualification_grads = 'Btech(ECE)';
@@ -19,22 +18,116 @@ class ProfilePageCardState extends State<ProfilePageCard> {
         context: context,
         builder: (context) {
           return AlertDialog(
-              title: Text('your email-id'), content: TextField(
-                onSubmitted:(String value){
-               _emailid =value;
-              },),
-             actions: <Widget>[FlatButton(child: Text('Submit'),
-              onPressed:() {
-                Navigator.pop(context);
-              },)],
-              );
+            title: Text('your email-id'),
+            content: TextField( decoration:InputDecoration(
+                hintText: 'enter email-id'),
+              onSubmitted: (String value) {
+              _emailid = value;
+            }),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  this.setState(() {
+                    _emailid ;
+                  });
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  _editNameDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('your Name'),
+            content: TextField(decoration: InputDecoration(hintText: 'enter name'),
+              onSubmitted: (String value) {
+                _name = value;
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  _editAgeDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('your Age'),
+            content: TextField(decoration: InputDecoration(hintText: 'enter age'),
+              onSubmitted: (String value) {
+                _age = value;
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  _editQualificationDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('your Qualification'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  TextField(decoration: InputDecoration(hintText: '10th'),
+                    onSubmitted: (String value) {
+                      _qualification10 = value;
+                    },
+                  ),
+                  TextField(decoration: InputDecoration(hintText: '12th'),
+                    onSubmitted: (String value) {
+                      _qualification12 = value;
+                    },
+                  ),
+                  TextField(decoration: InputDecoration(hintText: 'Graduation'),
+                    onSubmitted: (String value) {
+                      _qualification_grads = value;
+                    },
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
         });
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5.0,
+      elevation: 7.0,
       child: Column(
         children: <Widget>[
           Container(
@@ -44,7 +137,7 @@ class ProfilePageCardState extends State<ProfilePageCard> {
               children: <Widget>[
                 Text(
                   'Contact',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
@@ -58,8 +151,7 @@ class ProfilePageCardState extends State<ProfilePageCard> {
                       Text(
                         'Email-id',
                         style: TextStyle(
-                          fontSize: 16.0,
-                        ),
+                            fontSize: 18.0, fontWeight: FontWeight.bold),
                       ),
                       Spacer(),
                       IconButton(
@@ -77,7 +169,10 @@ class ProfilePageCardState extends State<ProfilePageCard> {
                     children: <Widget>[
                       Text(
                         '$_emailid',
-                        style: TextStyle(color: Colors.blueGrey),
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 16.0,
+                        ),
                       )
                     ],
                   )
@@ -92,19 +187,22 @@ class ProfilePageCardState extends State<ProfilePageCard> {
               children: <Widget>[
                 Text(
                   'Name:',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
                 ),
                 Spacer(),
                 Text(
                   '$_name',
-                  style: TextStyle(color: Colors.blueGrey),
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 16.0,
+                  ),
                 ),
                 Spacer(),
                 IconButton(
                   icon: Icon(Icons.edit, color: Colors.blueGrey),
-                  onPressed: () {},
+                  onPressed: () {
+                    _editNameDialog(context);
+                  },
                 )
               ],
             ),
@@ -115,19 +213,22 @@ class ProfilePageCardState extends State<ProfilePageCard> {
               children: <Widget>[
                 Text(
                   'Age:',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
                 ),
                 Spacer(),
                 Text(
                   '$_age',
-                  style: TextStyle(color: Colors.blueGrey),
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 16.0,
+                  ),
                 ),
                 Spacer(),
                 IconButton(
                   icon: Icon(Icons.edit, color: Colors.blueGrey),
-                  onPressed: () {},
+                  onPressed: () {
+                    _editAgeDialog(context);
+                  },
                 )
               ],
             ),
@@ -141,8 +242,7 @@ class ProfilePageCardState extends State<ProfilePageCard> {
                       Text(
                         'Qualifications',
                         style: TextStyle(
-                          fontSize: 16.0,
-                        ),
+                            fontSize: 18.0, fontWeight: FontWeight.bold),
                       ),
                       Spacer(),
                       IconButton(
@@ -150,24 +250,30 @@ class ProfilePageCardState extends State<ProfilePageCard> {
                           Icons.edit,
                           color: Colors.blueGrey,
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          _editQualificationDialog(context);
+                        },
                       )
                     ],
                   ),
                   Container(
-                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
                     child: Column(
                       children: <Widget>[
                         Row(
                           children: <Widget>[
                             Text(
                               '10th:',
-                              style: TextStyle(fontSize: 16.0),
+                              style: TextStyle(
+                                  fontSize: 16.0, fontWeight: FontWeight.w500),
                             ),
                             Spacer(),
                             Text(
                               ' $_qualification10',
-                              style: TextStyle(color: Colors.blueGrey),
+                              style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 16.0,
+                              ),
                             )
                           ],
                         ),
@@ -178,12 +284,16 @@ class ProfilePageCardState extends State<ProfilePageCard> {
                           children: <Widget>[
                             Text(
                               '12th:',
-                              style: TextStyle(fontSize: 16.0),
+                              style: TextStyle(
+                                  fontSize: 16.0, fontWeight: FontWeight.w500),
                             ),
                             Spacer(),
                             Text(
                               '$_qualification12',
-                              style: TextStyle(color: Colors.blueGrey),
+                              style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 16.0,
+                              ),
                             )
                           ],
                         ),
@@ -194,12 +304,16 @@ class ProfilePageCardState extends State<ProfilePageCard> {
                           children: <Widget>[
                             Text(
                               'Graduation:',
-                              style: TextStyle(fontSize: 16.0),
+                              style: TextStyle(
+                                  fontSize: 16.0, fontWeight: FontWeight.w500),
                             ),
                             Spacer(),
                             Text(
                               '$_qualification_grads',
-                              style: TextStyle(color: Colors.blueGrey),
+                              style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontSize: 16.0,
+                              ),
                             )
                           ],
                         ),
